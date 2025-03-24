ece5f59ff2ba352294a12c7983b2b89bae7417cd
Rajni Paswan <rajnipaswan140@gmail.com>
2025-03-24 21:59:08 +0530

    modify in main js file

--- my-react-vite-app/src/main.jsx
+++ my-react-vite-app/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>my name is Rajni</div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <MyApp />
+  </StrictMode>
 )
